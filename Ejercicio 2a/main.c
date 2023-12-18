4bacddad054fb9d589f6e2c396a8d6e4bcb6eda3
rodrigoyr <ryeperub@myuax.com>
2023-12-18 12:08:06 +0100

    code commit

--- Ejercicio 2a/main.c
+++ Ejercicio 2a/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir.\n");
+        return 1;
+    }
+
+    // Cerrar el archivo al final
+    fclose(file);
+
     return 0;
 }
