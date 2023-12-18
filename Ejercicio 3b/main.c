d951a4e648e7e4f7df1d0359e648841fd1d1637c
rodrigoyr <ryeperub@myuax.com>
2023-12-18 12:24:18 +0100

    code commit

--- Ejercicio 3b/main.c
+++ Ejercicio 3b/main.c
@@ -1,6 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *file = fopen("file.txt", "w");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para escritura.\n");
+        return 1;
+    }
+
+    fprintf(file, "Hola, mundo!\n");
+
+    // Cerrar el archivo después de escribir
+    fclose(file);
+
+    // Abrir de nuevo para leer
+    file = fopen("file.txt", "r");
+    if (file == NULL) {
+        printf("El archivo no se pudo abrir para lectura.\n");
+        return 1;
+    }
+
+    char buffer[50];
+    fscanf(file, "%s", buffer);
+
+    printf("Leído del archivo: %s\n", buffer);
+
+    // Cerrar el archivo después de leer
+    fclose(file);
+
     return 0;
 }
