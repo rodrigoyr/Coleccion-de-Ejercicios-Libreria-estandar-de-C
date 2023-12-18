0ae2e06ff27e4898088f4b8c1d859ef7b6783e99
rodrigoyr <ryeperub@myuax.com>
2023-12-18 11:55:19 +0100

    code commit

--- Ejercicio 1a/main.c
+++ Ejercicio 1a/main.c
@@ -1,6 +1,14 @@
+#include <stdlib.h>
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // Declarar un número negativo
+    int num = -10;
+
+    // Obtener el valor absoluto utilizando abs()
+    int abs_num = abs(num);
+
+    printf("El valor absoluto de %d es %d\n", num, abs_num);
+
     return 0;
 }
